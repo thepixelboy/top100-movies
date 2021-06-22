fa369a970c3e54500033a67e3074e90d7777a02a
Ruben <34570952+thepixelboy@users.noreply.github.com>
2021-06-22 21:01:40 +0200

    feat: save the movie list into a text file

--- main.py
+++ main.py
@@ -11,3 +11,7 @@ soup = BeautifulSoup(website_html, "html.parser")
 all_movies = soup.find_all(name="h3", class_="title")
 movie_titles = [movie.getText() for movie in all_movies]
 movies = movie_titles[::-1]
+
+with open("movies.txt", mode="w") as file:
+    for movie in movies:
+        file.write(f"{movie}\n")
