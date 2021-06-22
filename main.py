47367c0a7919edaf48b8897b4f8484bac780dd54
Ruben <34570952+thepixelboy@users.noreply.github.com>
2021-06-22 20:40:00 +0200

    feat: set up requests and beautiful soup

---
+++ main.py
@@ -0,0 +1,9 @@
+import requests
+from bs4 import BeautifulSoup
+
+URL = "https://www.empireonline.com/movies/features/best-movies-2/"
+
+response = requests.get(URL)
+website_html = response.text
+
+soup = BeautifulSoup(website_html, "html.parser")
